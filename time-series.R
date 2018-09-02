9a3f2f48c0aa2fa2239286f8277eb76e0cb2a609
josearun85 <josearun85@gmail.com>
2018-09-02 07:07:51 +0530

    Add files via upload

--- time-series.R
+++ time-series.R
@@ -1,3 +1,42 @@
+library(forecast)
+df3<-austres
+plot(df3)
+plot(decompose(df3))
+
+dcp <- decompose(df3)
+dcp$trend
+dcp$seasonal
+df <- data("taylor")
+df <- taylor
+
+plot(df)
+
+plot(decompose(df))
+
+df2 <- AirPassengers
+plot(df2)
+plot(decompose(df2))
+
+
+plot(meanf(df3,h=10))
+plot(naive(df3,h=10))
+plot(snaive(df3,h=10))
+plot(rwf(df3,drift = T,h=10))
+
+
+fit1 <- ses(df3)
+fcast1 <- forecast(fit1,h=10)
+plot(fcast1)
+
+
+fit <- HoltWinters(df3)
+fcast <- forecast(fit,h=10)
+plot(fcast)
+fcast
+summary(fit)
+summary(fit1)
+
+
 library(jsonlite)
 
 data <- fromJSON("https://exactspace.co/sensordata/profile1")
